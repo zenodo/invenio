31d1cb43c8f1ef512c547f1b2ff07142852f64a7
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2015-06-12 16:28:38 +0200

    formatter: silent recid type error
    * Changes error issued by BibFormatObject when given an non-integer
      recid to a warning.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- invenio/modules/formatter/engine.py
+++ invenio/modules/formatter/engine.py
@@ -2023,8 +2023,9 @@ class BibFormatObject(object):
         try:
             assert isinstance(recID, (int, long, type(None))), 'Argument of wrong type!'
         except AssertionError:
-            register_exception(prefix="recid needs to be an integer in BibFormatObject",
-                               alert_admin=False)
+            current_app.logger.warning(
+                "Record ID needs to be an integer in BibFormatObject"
+            )
             recID = int(recID)
         self.recID = recID
         self.lang = wash_language(ln)
